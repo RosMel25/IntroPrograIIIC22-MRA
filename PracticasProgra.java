55601a99b93a9268b38d72bdb78e4d24ca0651b2
RosMel25 <114128481+RosMel25@users.noreply.github.com>
2022-10-06 01:34:02 -0600

    Add files via upload

---
+++ PracticasProgra.java
@@ -0,0 +1,14 @@
+package practicasprogra;
+
+/**
+ *
+ * @author Dell
+ */
+public class PracticasProgra {
+
+    public static void main(String[] args) {
+        PracticasProgra02.practica02();
+        // PracticasProgra03.practica03();
+    }
+
+}
